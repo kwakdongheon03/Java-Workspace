773e629ac6755b632715cdedf981d39a19717d1b
Cat-Hanbit <hello.git.github@gmail.com>
2025-09-03 13:11:41 +0900

    hellojava 파일 commit 함.

---
+++ HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+    /**
+     * The main method, the entry point of the program.
+     * @param args Command line arguments (not used in this program).
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
